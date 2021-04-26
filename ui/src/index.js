0f1a7ab2cf4fe3fe2c86f59a5db4dd2b47feb80a
Bradley Shellnut <bradleyshellnut@protonmail.com>
2021-04-26 16:03:44 -0700

    Creating 2fa.

--- ui/src/index.js
+++ ui/src/index.js
@@ -17,6 +17,8 @@ async function startApp() {
       root: path.join(__dirname, "public"),
     })
 
+    app.get('/2fa', {}, async (request, reply) => reply.sendFile('2fa.html'))
+
     app.get('/reset/:email/:exp/:token', {}, async (request, reply) => reply.sendFile('reset.html'))
 
     app.get('/verify/:email/:token', {}, async ( request, reply ) => {
